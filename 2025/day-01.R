70851f09a600b35f76d0d6cecc33701a3d734b3b
Nicola Rennie <42338476+nrennie@users.noreply.github.com>
2025-12-01 14:20:03 +0000

    add day 1 part 1

---
+++ 2025/day-01.R
@@ -0,0 +1,34 @@
+
+library(dplyr)
+library(stringr)
+library(readr)
+
+
+# Data --------------------------------------------------------------------
+
+input <- readLines("2025/data/input.txt")
+
+
+# Process -----------------------------------------------------------------
+
+input_vec <- input |> 
+  as_tibble() |> 
+  mutate(number = if_else(
+    str_starts(value, "L"), -1 * parse_number(value), parse_number(value)
+  )) |> 
+  pull(number)
+
+
+# Part 1 ------------------------------------------------------------------
+
+num_zeros <- 0
+current <- 50
+
+for (i in 1:length(input_vec)) {
+  current <- (current + input_vec[i]) %% 100
+  if (current == 0) {
+    num_zeros <- num_zeros + 1
+  }
+}
+num_zeros
+
